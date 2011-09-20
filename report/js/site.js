148e2d7f7211a1c9fc0c473d6b01be32f633bc09
Hideyuki Saito <heeeeeeemail@gmail.com>
2011-09-20 14:13:53 +0900

    reoprt/report01.htmlにクロスフェードエフェクトを実装したよ

--- report/js/site.js
+++ report/js/site.js
@@ -1,4 +1,17 @@
 $(function() {
-  alert('foo');
-  $("#gnav ul li a").blend();
+  $('#gnav ul li a').blend();
+  
+  var interval = setInterval(function() {
+    var prev = $('.fadeitem.current')
+      , next = prev.next().length ? prev.next() : $('.fadeitem')[0];
+    
+    $(next).crossFade(prev, {
+      callbackIn: function() {
+        $(prev).removeClass('current');
+        $(next).addClass('current');
+      },
+      speedIn: 1000,
+      speedOut: 1000
+    });
+  }, 4000);
 });
