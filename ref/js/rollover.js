dc95326384a078296ffc760ab513b23165649c85
yui <yui@uniba.jp>
2011-09-20 20:20:51 +0900

    街コンテンツ追加、gnav修正

--- ref/js/rollover.js
+++ ref/js/rollover.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	var postfix = '_o';
-	$('#gnav a img, #howtogo .subnav a img, #report-top .mapBox .btnArea a img, .grid237 a img, .r672Box a img, .grid324 a img, .columnBox .grid208 a img').not('[src*="'+ postfix +'."]').each(function() {
+	$('#gnav a img, #howtogo .subnav a img, #topics .subnav a img, #report-top .mapBox .btnArea a img, .grid237 a img, .r672Box a img, .grid324 a img, .columnBox .grid208 a img').not('[src*="'+ postfix +'."]').each(function() {
 		var img = $(this);
 		var src = img.attr('src');
 		var src_hover = src.substr(0, src.lastIndexOf('.'))
