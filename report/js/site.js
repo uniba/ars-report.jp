345454dbf36569fb2baede55477ee305a173dea5
Hideyuki Saito <heeeeeeemail@gmail.com>
2011-09-21 16:18:02 +0900

    カルーセルとクロスフェードの実装完了

--- report/js/site.js
+++ report/js/site.js
@@ -1,21 +1,6 @@
 $(function() {
   $('#gnav ul li a').blend();
   
-  var interval = setInterval(function() {
-    var prev = $('.fadeitem.current')
-      , next = prev.next().length ? prev.next() : $('.fadeitem')[0];
-    
-    $(next).crossFade(prev, {
-      callbackIn: function() {
-        $(prev).removeClass('current');
-        $(next).addClass('current');
-      },
-      speedIn: 1000,
-      speedOut: 1000
-    });
-  }, 4000);
-  
-  
   var arrowContainer = '.arrowRight'
     , thumbsPerPage  = 8
     , thumbBox       = $('.thumbBox > ul')
@@ -23,18 +8,39 @@ $(function() {
                          ? Math.floor(thumbBox.children().length / thumbsPerPage + 1)
                          : thumbBox.children().length / thumbsPerPage
     , pageCount      = 0
-    , thumbArrow
-    , src
-    , src_hover;
+    , thumbArrow;
+  
+  $('.thumbBox img').each(function(i) {
+  	$(this).mouseover(function() {
+  	  var src = this.src
+  	    , src_hover = getHoverSrc(src);
+  	  this.src = src_hover;
+  	  $(this)
+  	    .click(function() {
+  	      var prev = $('.fadeitem.current')
+  	        , next = $('.fadeitem')[i];
+  	      
+  	      $(next).crossFade(prev, {
+  	      	callbackIn: function() {
+  	      	  $(prev).removeClass('current');
+  	      	  $(next).addClass('current');
+  	      	},
+  	      	speedIn: 500,
+  	      	speedOut: 500
+  	      });
+  	    })
+  	    .mouseout(function() {
+  	  	  this.src = src;
+  	    });
+  	});
+  });
   
   function arrowMouseDownHandler(e) {
     $(e.target).one('mouseup', arrowMouseUpHandler);
    
   	thumbArrow = $(this).children('img');
-  	src        = thumbArrow.attr('src');
-  	src_hover  = src.substr(0, src.lastIndexOf('.'))
-  	           + (src.indexOf('_o') != -1 ? '_o' : '')
-  	           + src.substring(src.lastIndexOf('.'));
+  	
+  	var src_hover  = getHoverSrc(thumbArrow.attr('src'));
   	
   	thumbArrow.attr('src', src_hover);
   	
@@ -44,20 +50,17 @@ $(function() {
   	  thumbBox
   	    .animate({
   	  	  'marginLeft': '-' + 592 * Math.min(numPages, pageCount) + 'px'
-  	  	}, 2000);
+  	  	}, 500);
   	} else {
   	  pageCount = Math.max(0, --pageCount);
   	  thumbBox
   	    .animate({
   	  	  'marginLeft': '+=' + 592 + 'px'
-  	  	}, 2000);
+  	  	}, 500);
   	 }
   }
   
   function arrowMouseUpHandler(e) {
-  	
-  	src = src_hover = '';
-  	
   	if (0 < pageCount && numPages - 1 > pageCount) {
   	  $('.arrowLeft')
   	   .one('mousedown', arrowMouseDownHandler)
@@ -89,4 +92,10 @@ $(function() {
   
   $('#thumbnail > ' + arrowContainer)
     .one('mousedown', arrowMouseDownHandler);
+  
+  function getHoverSrc(srcstr) {
+  	return srcstr.substr(0, srcstr.lastIndexOf('.'))
+  	     + (srcstr.indexOf('_o') == -1 ? '_o' : '')
+     	 + srcstr.substring(srcstr.lastIndexOf('.'));
+  }
 });
